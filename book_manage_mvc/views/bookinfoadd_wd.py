e6a1a2a6274e567524a064bcce9bac8b4306ddc6
zhou <429209708@qq.com>
2024-06-16 11:11:50 +0800

    v1

--- book_manage_mvc/views/bookinfoadd_wd.py
+++ book_manage_mvc/views/bookinfoadd_wd.py
@@ -1,15 +1,17 @@
 from book_manage_mvc.views.UI.bookinfoadd import Ui_Form
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
+from PyQt5.QtCore import pyqtSignal
 import sys
 
 
 class BookInfoAdd_wd(QWidget, Ui_Form):
+    signal_info = pyqtSignal(str, str, str, str, str)
     def __init__(self, parent=None, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
         self.setupUi(self)
 
     def add_book(self):
-        pass
+        self.signal_info.emit(self.bookname_le.text(), self.bookauthor_le.text(), self.bookprice_le.text(), self.booktype_cb.currentData(), self.bookdesc_pte.toPlainText())
 
     def reset(self):
         pass
